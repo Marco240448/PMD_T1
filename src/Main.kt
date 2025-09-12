6505bb5a0a9015d54e57b64365c48f9bcf58e286
Marco <marco.farina.balaga@iestubalcain.net>
2025-09-12 12:04:14 +0200

    Example 1 Created

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Marco"
+    println("Hola, $name!")
+
+    for (i in 1..5)
+        println("i = $i")
+}
