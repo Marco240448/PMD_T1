64d281c9fff70035f8448bc478b4f40d3e35e987
Marco <marco.farina.balaga@iestubalcain.net>
2025-09-30 08:58:16 +0200

    Updated

--- src/ejercicios/Ejercicio5.kt
+++ src/ejercicios/Ejercicio5.kt
@@ -1,30 +1,23 @@
 package ejercicios
 
 /*
- * Escribe una función que reciba dos palabras  y retorne verdadero o falso  según sean o no anagramas.
+ * Escribe una función que reciba dos palabras y retorne verdadero o falso según sean o no anagramas.
  * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
  * Dos palabras exactamente iguales no son anagrama.
  *
  * Casos de prueba:
  *
  * Roma - Amor -> true
- * Amor - Amor -> false
+ * Amol - Amor -> false
  * Amor - Amol -> false
  *
  */
 
-fun main() {
-    println(Comprobador("roMa", "Amor"))
-}
-
-private fun Comprobador(Pal1 : String, Pal2 : String) : Boolean {
-    var NPal = ""
-    for (index in 1..Pal1.length) {
-        NPal += Pal1[Pal1.length - index]
+fun main() = println(comprobador(",,,,", ",,,,"))
+private fun comprobador(pal1 : String, pal2 : String) : Boolean {
+    var nPal = ""
+    for (index in 1..pal1.length) {
+        nPal += pal1[pal1.length - index]
     }
-    if (NPal.toLowerCase() == Pal2.toLowerCase()) {
-        return true
-    } else {
-        return false
-    }
+    return nPal.lowercase() == pal2.lowercase()
 }
---
+++ src/ejercicios/Ejercicio7.kt
@@ -0,0 +1,23 @@
+package ejercicios
+
+/*
+ * Enunciado:
+ *
+ * Escribe un programa en Kotlin que realice lo siguiente:
+ *
+ * Crea una lista llamada nombres que contenga al menos 5 nombres de personas.
+ *
+ * Escribe una función llamada longitudPromedio que tome la lista de nombres como parámetro
+ * y devuelva la longitud promedio de los nombres en la lista como un número decimal
+ * (puede haber nombres de diferentes longitudes en la lista).
+ *
+ * Llama a la función longit
+ */
+
+fun main() {
+    val names = listOf("Ana", "Juan", "María", "Luis", "Elena")
+    fun longitudPromedio(namesList : List<String>) : Double { if (names.isEmpty()) { return 0.0 }
+        return namesList.sumOf {it.length}.toDouble() / namesList.size
+    }
+    print(longitudPromedio(names))
+}
